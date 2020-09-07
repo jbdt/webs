74c4d58566cf8f3f8d5006850bf37cb9eefda90e
Javier Blanco <jblanco@entelgy.com>
2020-09-07 15:43:18 +0200

    Una web en el public

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string(255)
-#  role                   :integer          default(0), not null
+#  role                   :integer          default("admin"), not null
 #  sign_in_count          :integer          default(0), not null
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
