cca131aaf516fdd7e92220db9879a267ac8346db
jblanco <jblanco>
2020-09-13 18:51:11 +0200

    Ver usuario

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,11 +2,35 @@ module UsersHelper
   def pretty_role(role)
     case role
     when 'admin'
-      'Administrador'
+      '<div class="mb-2 mr-2 badge badge-primary">Administrador</div>'.html_safe
     when 'integrator'
-      'Integrador'
+      '<div class="mb-2 mr-2 badge badge-success">Integrador</div>'.html_safe
     when 'client'
-      'Cliente'
+      '<div class="mb-2 mr-2 badge badge-alternate">Cliente</div>'.html_safe
+    end
+  end
+
+  def pretty_phone(phone)
+    "<a href=\"tel:#{phone}\">#{phone}</a>".html_safe
+  end
+
+  def pretty_ip(ip)
+    ip || 'Desconocida'
+  end
+
+  def pretty_boolean_state(boolean)
+    if boolean
+      '<div class="mb-2 mr-2 badge badge-pill badge-success">Activado</div>'.html_safe
+    else
+      '<div class="mb-2 mr-2 badge badge-pill badge-danger">Desactivado</div>'.html_safe
+    end
+  end
+
+  def pretty_boolean_tabs(boolean)
+    if boolean
+      'En línea'
+    else
+      'Sombreado'
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 NAMES = %w(Oliver Jack Harry George Jacob Hiroshi Takashi Akira Daisuke Ren Abbas Bishr Gamal Hamza Jafar)
 SURNAMES = %w(González Rodríguez Gómez Fernández López Díaz Martínez Pérez García Sánchez Romero Sosa Torres Álvarez Ruiz Ramírez Flores Benítez Acosta Medina Herrera Suárez Aguirre)
-COUNT_FILLED = 47
+COUNT_FILLED = 46
 ROLE = %w(admin integrator client)
 
 unless User.find_by_email('admin@correo.com')
   User.create! do |u|
     u.name = "Daniel"
     u.surnames = "López García"
-    u.phone = "+34 684 15 98"
+    u.phone = "+34 684 15 98 85"
     u.email = 'admin@correo.com'
     u.password = 'Test1234'
     u.role = 'admin'
@@ -18,7 +18,7 @@ unless User.find_by_email('integrator@correo.com')
   User.create! do |u|
     u.name = "María Luisa"
     u.surnames = "Rodríguez del Cerro"
-    u.phone = "+34 669 25 91"
+    u.phone = "+34 669 25 91 97"
     u.email = 'integrator@correo.com'
     u.password = 'Test1234'
     u.role = 'integrator'
@@ -29,10 +29,21 @@ unless User.find_by_email('client@correo.com')
   User.create! do |u|
     u.name = "Marta"
     u.surnames = "Cuesta Fuentes"
-    u.phone = "+34 677 82 23"
+    u.phone = "+34 677 82 23 04"
     u.email = 'client@correo.com'
     u.password = 'Test1234'
     u.role = 'client'
+  end
+end
+
+unless User.find_by_email('amorchi@vegana.com')
+  User.create! do |u|
+    u.name = "Alejandra"
+    u.surnames = "Rivas Joza"
+    u.phone = "+34 647 18 53 82	"
+    u.email = 'amorchi@vegana.com'
+    u.password = 'Test1234'
+    u.role = 'admin'
   end
 end
 
@@ -43,7 +54,7 @@ COUNT_FILLED.times do |i|
     User.create! do |u|
       u.name = name
       u.surnames = "#{SURNAMES.sample} #{SURNAMES.sample}"
-      u.phone = "+34 6#{rand(0..99).to_s.rjust(2, "0")} #{rand(0..99).to_s.rjust(2, "0")} #{rand(0..99).to_s.rjust(2, "0")}"
+      u.phone = "+34 6#{rand(0..99).to_s.rjust(2, "0")} #{rand(0..99).to_s.rjust(2, "0")} #{rand(0..99).to_s.rjust(2, "0")} #{rand(0..99).to_s.rjust(2, "0")}"
       u.email = email
       u.password = 'Test1234'
       u.role = ROLE.sample
