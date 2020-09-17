fb9b8bd820fe7004127268bdaa26cffabf1f3a39
jblanco <jblanco>
2020-09-17 19:31:09 +0200

    Mejora loading page

--- vendor/assets/javascript/loading_screen.js
+++ vendor/assets/javascript/loading_screen.js
@@ -1,5 +1,13 @@
+setTimeout(function(){
+    if ($(".loading_screen_background").css('display') !== 'none') {
+        $(".loading_screen_gif").fadeIn();
+    }
+}, 1000);
 $( window ).bind('load',function() {
-    $(".loading_screen-spinner").fadeOut();
+    $(".loading_screen").fadeOut();
+    setTimeout(function(){
+        $(".loading_screen").fadeOut();
+    }, 1000);
     // $(document).ajaxStart(function(){
     //     $(".loading_screen-spinner").show();
     // });
